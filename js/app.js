45d161a6354a4c658b84a2d214bad62a45678af7
rifatjahan823 <rifatjahan823@gmail.com>
2022-03-10 22:12:45 +0600

    read more button solve

--- js/app.js
+++ js/app.js
@@ -27,7 +27,12 @@ const reportPost = (id) => {
 };
 
 const displayContent = (text) => {
-    return text.length < 30 ? 'text' : text.slice(0, 30) + "<span class='fw-bold'>... read more</span>";
+  if( text.length>30){
+    return   text.slice(0, 30) + "<span class='fw-bold'>... read more</span>" 
+  }
+  else if(text.length<30){
+    return text ;
+  }
 };
 
 const switchTab = (id) => {
